ac232fd008910513a77671f1cb90dae2eb55a556
Jhon Valencia <jhonjairo.1331@gmail.com>
2026-06-17 11:17:36 +0200

    Se usan las funciones (minilibX) para la gestión de los pixeles. mlx_xpm_file_to_image: guardamos el puntero de nuestro archivo .xmp al malloc interno de la minilibX. mlx_get_data_addr: Nos devuelve la dirección de memoria exacta donde empiezan los pixeles reales

---
+++ usage_mlx/load_texture.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_texture.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhvalenc <jhvalenc@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 11:27:14 by jhvalenc          #+#    #+#             */
+/*   Updated: 2026/06/03 12:37:29 by jhvalenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../cub3d.h"
+
+/*
+ * Crea un objeto imagen a partir de un archivo .xpm usando la mlx.
+ *
+ * Extrae la dirección de memoria cruda de los píxeles para que el motor
+ * de renderizado (Raycaster) pueda leer los colores directamente 
+ * desde la RAM (O(1)), sin necesidad de usar las funciones lentas de mlx.
+ *
+ * Además, guarda el puntero de la imagen (img_ptr) por referencia para
+ * poder liberarlo con mlx_destroy_image() al cerrar el programa.
+*/
+
+uint32_t	*load_texture(t_game *game, char *path, void **save_img_ptr)
+{
+	t_texture	tex;
+
+	*save_img_ptr = mlx_xpm_file_to_image(game->mlx_ptr, path, &tex.width,
+			&tex.height);
+	if (*save_img_ptr == NULL)
+		return (NULL);
+	tex.pixels = (uint32_t *)mlx_get_data_addr(*save_img_ptr, &tex.bpp,
+			&tex.size_line, &tex.endian);
+	return (tex.pixels);
+}
