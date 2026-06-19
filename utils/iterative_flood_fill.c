fbde3039e7871940f738f195ecee4a7019977848
Jhon Valencia <jhonjairo.1331@gmail.com>
2026-06-19 20:39:21 +0200

    Se ajusta la función para que pase la norminette

--- utils/iterative_flood_fill.c
+++ utils/iterative_flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: jhvalenc <jhvalenc@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/18 11:58:40 by jhvalenc          #+#    #+#             */
-/*   Updated: 2026/06/19 13:25:48 by jhvalenc         ###   ########.fr       */
+/*   Updated: 2026/06/19 20:31:10 by jhvalenc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,74 +27,72 @@ static t_u8	*ft_memcpy(t_u8 *s1, t_u8 *s2, int len)
 	return (start_s1);
 }
 
+static int	validate_and_fill(t_game *game, t_u8 *map, int x, int y)
+{
+	int	index;
+
+	if (x < 0 || x >= game->map_width || y < 0 || y >= game->map_height)
+		return (-1);
+	index = (y * game->map_width + x);
+	if (map[index] == '1' || map[index] == 'V')
+		return (0);
+	if (map[index] == ' ')
+		return (-1);
+	map[index] = 'V';
+	return (1);
+}
+
+static void	paint_and_expand(t_point *stack, int *s_size, int x, int y)
+{
+	stack[*s_size].x = x;
+	stack[*s_size].y = y - 1;
+	(*s_size)++;
+	stack[*s_size].x = x;
+	stack[*s_size].y = y + 1;
+	(*s_size)++;
+	stack[*s_size].x = x + 1;
+	stack[*s_size].y = y;
+	(*s_size)++;
+	stack[*s_size].x = x - 1;
+	stack[*s_size].y = y;
+	(*s_size)++;
+}
+
+static int	run_flood_fill(t_game *game, t_u8 *map_clone, t_point *stack)
+{
+	int	s_size;
+	int	x;
+	int	y;
+	int	status;
+
+	s_size = 0;
+	stack[s_size].x = (int)game->player_x;
+	stack[s_size++].y = (int)game->player_y;
+	while (s_size > 0)
+	{
+		s_size--;
+		x = stack[s_size].x;
+		y = stack[s_size].y;
+		status = validate_and_fill(game, map_clone, x, y);
+		if (status == -1)
+			return (err_msg("Limit flood fill", ERROR_LIMIT_FF, -1));
+		if (status == 1)
+			paint_and_expand(stack, &s_size, x, y);
+	}
+	return (0);
+}
+
 int	iteractive_flood_fill(t_game *game)
 {
 	t_u8	*map_clone;
 	t_point	*stack;
-	int		total_size;
-	int		stack_size;
-	int		current_x;
-	int		current_y;
-	int		index;
+	int		ret;
 
-	total_size = game->map_width * game->map_height;
-	map_clone = (t_u8 *)malloc(sizeof(t_u8) * total_size);
-	if (map_clone == NULL)
-		return (-1);
-	// copiar byte a byte desde game->map hacia map_clone
-	ft_memcpy(map_clone, game->map, total_size);
-	stack = (t_point *)malloc(sizeof(t_point) * total_size);
-	if (stack == NULL)
-	{
-		free(map_clone);
-		return (-1);
-	}
-	stack_size = 0;
-	stack[stack_size].x = (int)game->player_x;
-	stack[stack_size].y = (int)game->player_y;
-	stack_size++;
-	// Fase 2:
-	while (stack_size > 0)
-	{
-		// extraer (pop)
-		stack_size--;
-		current_x = stack[stack_size].x;
-		current_y = stack[stack_size].y;
-		// Defensa de los limites
-		if (current_x < 0 || current_x >= game->map_width || current_y < 0
-			|| current_y >= game->map_height)
-		{
-			free(map_clone);
-			free(stack);
-			return (err_msg("Limit flood fill", ERROR_LIMIT_FF, -1));
-		}
-		// Calcular el índice
-		index = (current_y * game->map_width + current_x);
-		// Evaluar la casilla
-		if (map_clone[index] == '1' || map_clone[index] == 'V')
-			continue ;
-		if (map_clone[index] == ' ')
-		{
-			free(map_clone);
-			free(stack);
-			return (err_msg("Limit flood fill", ERROR_LIMIT_FF, -1));
-		}
-		// Pintar y expandir
-		map_clone[index] = 'V';
-		stack[stack_size].x = current_x;
-		stack[stack_size].y = current_y - 1;
-		stack_size++;
-		stack[stack_size].x = current_x;
-		stack[stack_size].y = current_y + 1;
-		stack_size++;
-		stack[stack_size].x = current_x + 1;
-		stack[stack_size].y = current_y;
-		stack_size++;
-		stack[stack_size].x = current_x - 1;
-		stack[stack_size].y = current_y;
-		stack_size++;
-	}
-	free(map_clone);
-	free(stack);
-	return (0);
+	map_clone = malloc(sizeof(t_u8) * (game->map_width * game->map_height));
+	stack = malloc(sizeof(t_point) * (game->map_width * game->map_height));
+	if (stack == NULL || map_clone == NULL)
+		return (free_and_return(map_clone, stack, -1));
+	ft_memcpy(map_clone, game->map, game->map_width * game->map_height);
+	ret = run_flood_fill(game, map_clone, stack);
+	return (free_and_return(map_clone, stack, ret));
 }
